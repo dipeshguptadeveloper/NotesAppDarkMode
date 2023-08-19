c0174c2e8d55ba469ebedc33660dce7e27ab54d1
dipeshgupta <dipeshguptadeveloper@gmail.com>
2023-08-19 18:54:20 +0530

    few changes in search note UI

--- app/src/main/java/com/dkgtech/notesappdarkmode/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/dkgtech/notesappdarkmode/ui/fragments/HomeFragment.kt
@@ -90,6 +90,17 @@ class HomeFragment : Fragment() {
                 }
             }
 
+            edtSearch.setOnClickListener {
+                btnSearch.visibility = View.GONE
+                btnClose.visibility = View.VISIBLE
+            }
+
+            btnClose.setOnClickListener {
+                edtSearch.setText("")
+                btnClose.visibility = View.GONE
+                btnSearch.visibility = View.VISIBLE
+            }
+
             edtSearch.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 }
